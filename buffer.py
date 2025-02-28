baa684f55471e53223ab66741f22ce654a38e6ba
Ronit <ronitkataria450@gmail.com>
2025-02-28 22:40:27 +0500

    sohail bhai work done, khuda ko mano sone do mujhe

--- buffer.py
+++ buffer.py
@@ -13,7 +13,7 @@ import math
 class Cleaner:
     def __init__(self, json_folder_path, detect_output_folder, box_size, csv_path, field_json) -> None:
         self.json_folder_path = json_folder_path
-        self.detect_out = os.path.join(detect_output_folder, 'geojsons')
+        self.detect_out =detect_output_folder
         self.box_size = box_size
         self.data = pd.read_csv(csv_path)
         self.post_obj = PostProcess()
@@ -21,6 +21,7 @@ class Cleaner:
         self.field_json = field_json
         self.results = []
         self.emerged_pop_count = []
+        self.geojson_output = os.path.join(self.detect_out, 'geojsons')
         os.makedirs(self.detect_out, exist_ok=True)
                     
         
@@ -31,7 +32,7 @@ class Cleaner:
         try:
             start_time = time.time()
             json_path = os.path.join(self.json_folder_path, file)
-            shp_output_base = os.path.join(self.detect_out, f"{os.path.splitext(file)[0]}.geojson")
+            shp_output_base = os.path.join(self.geojson_output, f"{os.path.splitext(file)[0]}.geojson")
             gsd, width, height, count = self.post_obj.main(
                 json_path=json_path,
                 box_size=self.box_size,
@@ -63,22 +64,41 @@ class Cleaner:
                 except Exception as e:
                     print(f"Error in thread: {str(e)}")
         
-        self.analysis_feild_dict = self.analysis_obj.generate_field_analysis(self.emerged_pop_count)
-        self.json_maker()
+        self.analysis_field_dict = self.analysis_obj.generate_field_analysis(self.emerged_pop_count)
+        self.json_csv_maker()
     
     
-    def json_maker(self):
+    def json_csv_maker(self):
         result_folder = os.path.join(self.detect_out, 'Analysis')
         os.makedirs(result_folder, exist_ok=True )
         final_json = []
-        final_json.append(self.analysis_feild_dict)
+        final_json.append(self.analysis_field_dict)
         final_json.append(self.results)
-        
         json_path = os.path.join(result_folder, 'analysis.json')
+        csv_path = os.path.join(result_folder, 'final_analysis.csv')
         with open(json_path, "w", encoding="utf-8") as json_file:
             json.dump(final_json, json_file, indent=4)
-            
-        print(f"JSON successfully written to {json_path}")
+        
+        
+        df = pd.DataFrame([{
+        "company": self.analysis_field_dict.get("company", ""),
+        "fieldId": self.analysis_field_dict.get("field_id", ""),
+        "boundary": f"{self.analysis_field_dict.get('boundary_acres', '')} Acres",  
+        "cropType": self.analysis_field_dict.get("crop_type", ""),
+        "plantationDate": self.analysis_field_dict.get("plantation_date", ""),
+        "flightScan": self.analysis_field_dict.get("flight_scan_date", ""),
+        "seededArea": self.analysis_field_dict.get("total_crop_area_acres", ""),
+        "targetPopulation": self.analysis_field_dict.get("total_target_plants", ""),
+        "emergedPopulation": self.analysis_field_dict.get("total_emerged_plants", ""),
+        "emergenceRate": self.analysis_field_dict.get("emergence_rate", ""),
+        "yieldLossPlants": self.analysis_field_dict.get("yield_loss_plants", ""),
+        "yieldLossPercentage": self.analysis_field_dict.get("yield_loss_percentage", ""),
+        "perAcreTarget": self.analysis_field_dict.get("target_population_per_acre", ""),
+        "perAcreEmerged": self.analysis_field_dict.get("emerged_population_per_acre", "")
+    }])
+        
+        df.to_csv(csv_path, index=False)
+        print(f"JSON and CSV successfully Done")
         
         
         
